5f6c555f6db8f1751b01f2d53741c998a2660ee9
pantho0 <panthomashreky6@gmail.com>
2024-12-10 13:40:51 +0600

    controlled form added for react hook form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Button, Row } from "antd";
 import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
@@ -23,31 +23,31 @@ const Login = () => {
 
   const onSubmit = async (data: FieldValues) => {
     console.log(data);
-    // const toastID = toast.loading("Logging In");
-    // try {
-    //   const userInfo = {
-    //     id: data.id,
-    //     password: data.password,
-    //   };
-    //   const res = await login(userInfo).unwrap();
-    //   const user = verifyToken(res.data.accessToken) as TUser;
-    //   dispatch(setUser({ user: user, token: res.data.accessToken }));
-    //   navigate(`/${user.role}/dashboard`);
-    //   toast.success("Logged In", { id: toastID, duration: 2000 });
-    // } catch (error) {
-    //   toast.error("something went wrong", { id: toastID, duration: 2000 });
-    // }
+    const toastID = toast.loading("Logging In");
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken) as TUser;
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      navigate(`/${user.role}/dashboard`);
+      toast.success("Logged In", { id: toastID, duration: 2000 });
+    } catch (error) {
+      toast.error("something went wrong", { id: toastID, duration: 2000 });
+    }
   };
   return (
-    <PHForm onSubmit={onSubmit}>
-      <div>
+    <Row justify="center" align="middle" style={{ height: "100vh" }}>
+      <PHForm onSubmit={onSubmit}>
         <PHInput type="text" name="id" label="ID:" />
-      </div>
-      <div>
+
         <PHInput type="text" name="password" label="Password:" />
-      </div>
-      <Button htmlType="submit">Login</Button>
-    </PHForm>
+
+        <Button htmlType="submit">Login</Button>
+      </PHForm>
+    </Row>
   );
 };
 
