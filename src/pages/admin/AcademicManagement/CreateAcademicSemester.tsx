5c11c4388c8bad3843c89498ee5e492f3d3de6c2
pantho0 <panthomashreky6@gmail.com>
2024-12-10 16:21:15 +0600

    semster form added with all select options

--- src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
@@ -1,20 +1,42 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
-import PHForm from "../../../components/form/PHform";
-import PHInput from "../../../components/form/PHInput";
+
 import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm.tsx";
 import PHSelect from "../../../components/form/PHSelect";
+import { nameOptions } from "../../../contants/semester.ts";
+import { monthOptions } from "../../../contants/globals.ts";
+
+const currentYear = new Date().getFullYear();
+const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
+  value: String(currentYear + number),
+  label: String(currentYear + number),
+}));
 
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+    const name = nameOptions[Number(data?.name) - 1]?.label;
+    const semsterData = {
+      name,
+      code: data?.name,
+      year: data?.year,
+      startMonth: data?.startMonth,
+      endMonth: data?.endMonth,
+    };
+    console.log(semsterData);
   };
+
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-          <PHInput type="text" name="name" label="name" />
-          <PHInput type="text" name="year" label="year" />
-          <PHSelect label={"name"} />
+          <PHSelect label={"Name"} name={"name"} options={nameOptions} />
+          <PHSelect label="Year" name="year" options={yearOptions} />
+          <PHSelect
+            label="Start Month"
+            name="startMonth"
+            options={monthOptions}
+          />
+          <PHSelect label="End Month" name="endMonth" options={monthOptions} />
           <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
